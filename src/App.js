469f3aaddb725529940aae5502cf2a16536698d6
Peterson Santos <petersonkfs@outlook.com>
2022-10-27 21:36:40 -0300

    alterações bases

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import './styles.css';
 
 function App() {
+
+  const [conta, setConta] = useState('');
+  const [gorjeta, setGorjeta] = useState('');
+
+  let answer = document.querySelector('.answer');
+  answer.innerHTML = `<p>Sub-total R$ ${conta}</p>`;
+
+  const handleInput = (e)=>{
+    setConta(e.target.value);
+  }
+
+  const handleInputGJT = (e)=>{
+    setGorjeta(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+
+      <div className='title'>
+        Calculadora de Gorjeta
+      </div>
+
+      <div className='content'>
+        <p>Quanto deu a conta?</p>
+        <input
+        type="number"
+        className='iConta'
+        value={conta}
+        onChange={handleInput}
+        />
+
+        <p>Qual a porcentagem da gorjeta?</p>
+        <input
+        type="number"
+        className='iGorjeta'
+        value={gorjeta}
+        onChange={handleInputGJT}
+        />
+      </div>
+
+      <div className='line'>
+        <div></div>
+      </div>
+
+      <div className='answer'></div>
+
     </div>
   );
 }
